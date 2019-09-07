c4fe412e82d5a828701a3e3a72c96b11fc08c2f1
Ben Wheat <bwheatie@gmail.com>
2019-09-06 20:21:06 -0500

    Adds layout struct and matches component autolayout file

--- lib/layouts/primitives/autolayout.ex
+++ lib/layouts/primitives/autolayout.ex
@@ -7,6 +7,14 @@ defmodule Scenic.Layouts.Primitives.AutoLayout do
 
   import Scenic.Primitives
 
+  defmodule Layout do
+    defstruct component: %Scenic.Primitive{},
+              starting_xy: {},
+              max_xy: {},
+              grid_xy: {},
+              graph: %{}
+  end
+
   def layout(graph, group_id, list_of_prim_ids) do
     rect_id =
       group_id
@@ -16,7 +24,7 @@ defmodule Scenic.Layouts.Primitives.AutoLayout do
       |> String.to_atom()
 
     [%{transforms: %{translate: grid_xy}}] = Graph.get(graph, group_id)
-    [%{data: max_xy}] = Graph.get(graph, rect_id) |> IO.inspect
+    [%{data: max_xy}] = Graph.get(graph, rect_id) |> IO.inspect()
 
     graph =
       Enum.reduce(list_of_prim_ids, [], fn p_id, acc ->
@@ -30,69 +38,67 @@ defmodule Scenic.Layouts.Primitives.AutoLayout do
             _ ->
               acc
           end
+      layout = %Layout{
+          component: component,
+          starting_xy: starting_xy,
+          max_xy: max_xy,
+          grid_xy: grid_xy,
+          graph: graph
+        }
 
-        case module do
-          Scenic.Primitive.Arc ->
-            nil
-
-          Scenic.Primitive.Circle ->
-            case Circle.translate(primitive, max_xy, starting_xy, grid_xy) do
-              {:ok, xy} ->
-                new_graph = Graph.modify(graph, p_id, &update_opts(&1, t: xy))
-                {xy, new_graph}
-
-              {:error, error} ->
-                {:error, error}
-            end
-
-          Scenic.Primitive.Rectangle ->
-            case Rectangle.translate(primitive, max_xy, starting_xy, grid_xy) do
-              {:ok, xy} ->
-                new_graph = Graph.modify(graph, p_id, &update_opts(&1, t: xy))
-                {xy, new_graph}
-
-              {:error, error} ->
-                {:error, error}
-            end
-
-          Scenic.Primitive.RoundedRectangle ->
-            case RoundedRectangle.translate(primitive, max_xy, starting_xy, grid_xy) do
-              {:ok, xy} ->
-                new_graph = Graph.modify(graph, p_id, &update_opts(&1, t: xy))
-                {xy, new_graph}
-
-              {:error, error} ->
-                {:error, error}
-            end
-
-          Scenic.Primitive.Line ->
-            nil
-
-          Scenic.Primitive.Path ->
-            nil
-
-          Scenic.Primitive.Quad ->
-            nil
-
-          Scenic.Primitive.Sector ->
-            nil
-
-          Scenic.Primitive.Triangle ->
-            case Triangle.translate(primitive, max_xy, starting_xy, grid_xy) do
-              {:ok, xy} ->
-                new_graph = Graph.modify(graph, p_id, &update_opts(&1, t: xy))
-                {xy, new_graph}
-
-              {:error, error} ->
-                {:error, error}
-            end
-
-          _ ->
-            {:error, "Must be a primitive to auto-layout"}
-        end
+        do_layout(comp_type, layout, c_id)
       end)
       |> elem(1)
 
     {:ok, graph}
   end
+
+  defp do_layout(Scenic.Primitive.Arc, _layout, _p_id), do: nil
+  defp do_layout(Scenic.Primitive.Circle, layout, p_id) do
+    case Circle.translate(primitive, max_xy, starting_xy, grid_xy) do
+      {:ok, xy} ->
+        new_graph = Graph.modify(graph, p_id, &update_opts(&1, t: xy))
+        {xy, new_graph}
+
+      {:error, error} ->
+        {:error, error}
+    end
+
+  defp do_layout(Scenic.Primitive.Rectangle, layout, p_id) do
+    case Rectangle.translate(primitive, max_xy, starting_xy, grid_xy) do
+      {:ok, xy} ->
+        new_graph = Graph.modify(graph, p_id, &update_opts(&1, t: xy))
+        {xy, new_graph}
+
+      {:error, error} ->
+        {:error, error}
+    end
+  end
+
+  defp do_layout(Scenic.Primitive.RoundedRectangle, layout, p_id) do
+    case RoundedRectangle.translate(primitive, max_xy, starting_xy, grid_xy) do
+      {:ok, xy} ->
+        new_graph = Graph.modify(graph, p_id, &update_opts(&1, t: xy))
+        {xy, new_graph}
+
+      {:error, error} ->
+        {:error, error}
+    end
+  end
+
+  defp do_layout(Scenic.Primitive.Line, _layout, _p_id), do: nil
+  defp do_layout(Scenic.Primitive.Path, _layout, _p_id), do: nil
+  defp do_layout(Scenic.Primitive.Quad, _layout, _p_id), do: nil
+  defp do_layout(Scenic.Primitive.Sector, layout, p_id), do: nil
+  defp do_layout(Scenic.Primitive.Triangle, layout, p_id) do
+    case Triangle.translate(primitive, max_xy, starting_xy, grid_xy) do
+      {:ok, xy} ->
+        new_graph = Graph.modify(graph, p_id, &update_opts(&1, t: xy))
+        {xy, new_graph}
+
+      {:error, error} ->
+        {:error, error}
+    end
+  end
+  defp do_layout(_, _, _), do: {:error, "Must be a primitive to auto-layout"}
 end
