2ec134f1c68c0efa3f55dbfddff15cd5145dcae1
Ben Wheat <ben@blockfi.com>
2020-08-11 15:03:21 -0500

    Grids are the key to the layout-o-matic working for engineers but is confusing to use. The work here makes it clearer how to build a grid as well as adding center text positioning.

--- lib/layouts/grid.ex
+++ lib/layouts/grid.ex
@@ -6,41 +6,8 @@ defmodule LayoutOMatic.Grid do
 
   Grids allow you to segment a viewport much like a CSS grid. This allows for
   clear and symantic layouts. Creating a grid is as simple as passing a
-  %GridBuilder{} with some values and your viewport will have a grid ready to
-  be used.
-
-  ## Data
-  * `:viewport` - The viewport struct you want a grid drawn to.
-  * `:grid_template` - The type and size of columns for the grid. *Required field*.
-    * `{:equal, number_of_equal_columns}` - Indicates columns will be equally sized and how many of them to be drawn
-    * `{:percent, percentage_of_viewport}` - Indicates columns will be a percentage of the viewport and what percentage of the viewport. This option is a list of percentages which cannot exceed 100%.
-    * `{:relative, percentage_relative_to_object}` - Indicates columns will be drawn relative to another object. This could be used to draw a grid relative to another primitive of component as well as another grid.
-  * `:max_xy` - The maximum {x,y} the grid should fit into. This will likely be the viewport size in an inital graph. Default `{700, 600}`. This is the default viewport size for a new scenic app.
-  * `:starting_xy` - The {x,y} the grid should start at. Default is {0, 0}.
-  * `:grid_ids` - The ids used for each segment of the grid in order to recall the segment later in order to assign a list of objects to it for layouts. Symantically named ids is recommneded. *Required field*
-  * `:opts` - A list of additional options
-    * `:draw` - Boolean to determine if the grid should be drawn or not. Useful for making sure objects are falling where expected. Default is `false`.
-
-  ```
-  @viewport :scenic_layout_o_matic
-            |> Application.get_env(:viewport)
-            |> Map.get(:size)
-
-  @grid %GridBuilder{
-    grid_template: [{:equal, 2}],
-    max_xy: @viewport,
-    grid_ids: [:left, :right],
-    starting_xy: {0, 0}
-  }
-
-  @graph Graph.build()
-         |> add_specs_to_graph(Grid.grid(@grid),
-           id: :root_grid
-         )
-
-  def init(_, opts) do
-    {:ok, opts, push: @graph}
-  end
+  map with the following keys representing where to start the grid, where
+  to end the grid, grid_ids, and opts.
   """
 
   defmodule Error do
@@ -49,79 +16,147 @@ defmodule LayoutOMatic.Grid do
   end
 
   defmodule GridBuilder do
-    @enforce_keys [:grid_template, :grid_ids]
-    defstruct viewport: %{},
-              grid_template: [{:equal, 1}],
-              # This should come off the viewport map passed in.
-              max_xy: {700, 600},
-              starting_xy: {0, 0},
-              grid_ids: nil,
-              column_sizes: nil,
-              opts: [draw: false]
+    defstruct [
+      :viewport,
+      :max_xy,
+      :starting_xy,
+      :grid_ids,
+      :column_sizes,
+      :opts
+    ]
   end
 
-  @spec simple_grid({number, number}, {number, number}, [atom], [any]) :: [{number, number}]
-  def simple_grid(
-        {viewport_x, viewport_y},
-        {starting_x, starting_y} = starting_xy \\ {0, 0},
-        id_list \\ [:top, :bottom, :left, :right, :center],
-        opts \\ [draw: false]
-      ) do
-    top_bottom_size = {viewport_x, trunc(viewport_y / 2)}
-    top = {top_bottom_size, starting_xy}
-    bottom = {top_bottom_size, {starting_x, trunc(viewport_y / 2)}}
+  @doc """
+  A simple grid is the most basic type. It creates a grid with a top, bottom, left, right, center. All that is needed is the starting and max points.
+  The resulting grid will contain rects and groups per portion of grid with center being the exception. Center is simple a point which can be referrenced.
+  Note grids are primitives specs so to add them to the graph you will need to use `add_specs_to_graph/2`.
 
-    left_right_size = {trunc(viewport_x / 2), viewport_y}
+  * `:starting_xy` - The {x,y} the grid should start at.
+  * `:max_xy` - The maximum {x,y} the grid should fit into. This can be the full viewport of a subset of the viewport like another grid you are adding to.
+  * `:grid_ids` - List of atoms representing the ids to be added. These ids will be applied in a clockwise way. *Note:* it is important to use unique ids unless
+  the intention is to have similar components to be modified, deleted together. If this does not sound like what you want, be sure to use unique ids.
+  * `:opts` - A list of additional options
+    * `:draw` - Boolean to determine if the grid should be drawn or not. Useful for making sure objects are falling where expected.
+    * `:grid_ids` - The ids used for each segment of the grid in order to recall the segment later in order to assign a list of objects to it for layouts. Symantically named ids is recommneded.
+
+  ```elixir
+  def init(_, opts) do
+    grid = Graph.build()
+    |> add_specs_to_graph(Grid.simple({0, 0}, {700, 600}, [:top, :right, :bottom, :left, :center]))
+    {:ok, opts, push: graph}
+  end
+  ```
+  """
+  @spec simple({number, number}, {number, number}, [any]) :: [{number, number}]
+  def simple({starting_x, starting_y} = starting_xy, {max_x, max_y}, grid_ids, opts \\ []) do
+    top_bottom_size = {max_x, trunc(max_y / 2)}
+    top = {top_bottom_size, starting_xy}
+    bottom = {top_bottom_size, {starting_x, trunc(max_y / 2)}}
+
+    left_right_size = {trunc(max_x / 2), max_y}
     left = {left_right_size, starting_xy}
-    right = {left_right_size, {trunc(viewport_x / 2), starting_y}}
+    right = {left_right_size, {trunc(max_x / 2), starting_y}}
 
     # center should just be a point and the rect should ultimately do nothing.
-    center = {{0, 0}, {elem(left_right_size, 0), elem(top_bottom_size, 1)}}
+    center = {starting_xy, {elem(left_right_size, 0), elem(top_bottom_size, 1)}}
 
     grid = [
-      {Enum.fetch!(id_list, 0), top},
-      {Enum.fetch!(id_list, 1), bottom},
-      {Enum.fetch!(id_list, 2), left},
-      {Enum.fetch!(id_list, 3), right},
-      {Enum.fetch!(id_list, 4), center}
+      {Enum.fetch!(grid_ids, 0), top},
+      {Enum.fetch!(grid_ids, 1), right},
+      {Enum.fetch!(grid_ids, 2), bottom},
+      {Enum.fetch!(grid_ids, 3), left},
+      {Enum.fetch!(grid_ids, 4), center}
     ]
 
     Enum.map(grid, fn {id, coords} ->
-      build_grid(coords, id, opts[:draw])
+      draw_grid(coords, id, opts[:draw])
     end)
   end
 
-  @spec complex_grid(map) :: [{number, number}]
-  def complex_grid(%{} = grid) do
-    struct(GridBuilder, grid)
-    {starting_x, _} = Map.get(grid, :starting_xy)
-    {max_x, _} = Map.get(grid, :max_xy)
+  @doc """
+  A percentage grid is one where the grid portions are percentages o the overall grid area. Like a simple grid, percentages take a starting and max point but also
+  take a list of percentages representing the grid portions from left to right. Note the returned grid will be a list of primitive specs so you will need to use
+  `add_specs_to_graph/2`.
 
+  ```elixir
+  Grid.percentage({0, 0}, {700, 600}, [25, 50, 25], [:left, :center, :right], [draw: true])
+  ```
+  """
+  @spec percentage({number, number}, {number, number}, [number], [atom], [any]) :: [
+          Scenic.Primitives.Group.t()
+        ]
+  def percentage(
+        {starting_x, _} = starting_xy,
+        {max_x, _} = max_xy,
+        [] = percentages,
+        [] = grid_ids,
+        opts \\ []
+      ) do
     column_sizes =
-      Enum.map(Map.get(grid, :grid_template), fn t ->
-        case elem(t, 0) do
-          :percent ->
-            length = tuple_size(t) - 1
-
-            Enum.map(1..length, fn e ->
-              trunc(elem(t, e) / 100 * max_x - starting_x)
-            end)
-
-          :equal ->
-            Enum.map(1..elem(t, 1), fn _ ->
-              div(max_x, elem(t, 1))
-            end)
-
-          :relative ->
-            trunc(elem(t, 1) / 100 * max_x)
-        end
+      Enum.map(percentages, fn p ->
+        trunc(p / 100 * max_x - starting_x)
       end)
 
-    Map.put(
-      grid,
-      :column_sizes,
-      List.flatten(column_sizes)
-    )
+    %{
+      starting_xy: starting_xy,
+      max_xy: max_xy,
+      column_sizes: column_sizes,
+      grid_ids: grid_ids,
+      opts: opts
+    }
+    |> get_x_coordinates()
+  end
+
+  @doc """
+  A pixel grid is identical to a percentatge grid with the difference being that instead of a list of numbers representing percentages, that list represents pixels.
+  Like a simple grid, pixel take a starting and max point but also take a list of numbers representing the grid portions in pixel from left to right. Note the
+  returned grid will be a list of primitive specs so you will need to use `add_specs_to_graph/2`.
+
+  ```elixir
+  Grid.pixel({0, 0}, {700, 600}, [50, 150, 25], [:left_nav, :content, :side_bar], [draw: true])
+  ```
+  """
+
+  @spec pixel({number, number}, {number, number}, [number], [atom], []) :: [
+          Scenic.Primitives.Group.t()
+        ]
+  def pixel(starting_xy, max_xy, [] = sizes, [] = grid_ids, opts \\ []) do
+    %{
+      starting_xy: starting_xy,
+      max_xy: max_xy,
+      column_sizes: sizes,
+      grid_ids: grid_ids,
+      opts: opts
+    }
+    |> get_x_coordinates()
+  end
+
+  @doc """
+  An equal grid is much like a pixel grid but instead of determined sizes, it takes a number representing the equal number of portions to be created.
+  Like a simple grid, equal take a starting and max point but also take a number representing the equally sized grid portions from left to right. Note the
+  returned grid will be a list of primitive specs so you will need to use `add_specs_to_graph/2`.
+
+  ```elixir
+  Grid.equal({0, 0}, {700, 600}, 4, [:left, :left_center, :right_center, :right], [draw: true])
+  ```
+  """
+
+  @spec equal({number, number}, {number, number}, number, [atom], []) :: [
+          Scenic.Primitives.Group.t()
+        ]
+  def equal(starting_xy, {max_x, _} = max_xy, number_of_portions, grid_ids, opts \\ []) do
+    column_sizes =
+      Enum.map(1..number_of_portions, fn p ->
+        div(max_x, p)
+      end)
+
+    %{
+      starting_xy: starting_xy,
+      max_xy: max_xy,
+      column_sizes: column_sizes,
+      grid_ids: grid_ids,
+      opts: opts
+    }
     |> get_x_coordinates()
   end
 
@@ -136,11 +171,11 @@ defmodule LayoutOMatic.Grid do
 
       case acc do
         [] ->
-          {build_grid(max_y, elem(i, 1), starting_xy, elem(i, 0), opts[:draw]),
+          {draw_grid(max_y, elem(i, 1), starting_xy, elem(i, 0), opts[:draw]),
            elem(starting_xy, 0) + elem(i, 1)}
 
         _ ->
-          {build_grid(max_y, elem(i, 1), {acc, elem(starting_xy, 1)}, elem(i, 0), opts[:draw]),
+          {draw_grid(max_y, elem(i, 1), {acc, elem(starting_xy, 1)}, elem(i, 0), opts[:draw]),
            acc + elem(i, 1)}
       end
     end)
@@ -148,7 +183,7 @@ defmodule LayoutOMatic.Grid do
   end
 
   @doc false
-  defp build_grid({size, translate}, id, draw) do
+  defp draw_grid({size, translate}, id, draw) do
     group_spec(
       rect_spec(size,
         stroke: {1, :black},
@@ -162,7 +197,7 @@ defmodule LayoutOMatic.Grid do
   end
 
   @doc false
-  defp build_grid(max_y, size, starting_xy, id, draw) do
+  defp draw_grid(max_y, size, starting_xy, id, draw) do
     group_spec(
       rect_spec({size, max_y},
         stroke: {1, :black},
--- lib/layouts/primitives/text_positioning.ex
+++ lib/layouts/primitives/text_positioning.ex
@@ -1,4 +1,8 @@
 defmodule LayoutOMatic.TextPosition do
+  @moduledoc """
+  While Scenic supports text positioning within a component, it does not support positioning
+  based on a point. This works by passing a string and point to be postioned from.
+  """
   # Text are translated from the bottom left of the character
   @default_font_size 20
   @default_font :roboto
@@ -17,14 +21,17 @@ defmodule LayoutOMatic.TextPosition do
   # def right() do
   # end
 
+  @doc """
+  Centers string according to point.
+  """
   @spec center(String.t(), {number, number}) :: {number, number}
-  def center(string, {x, y}, font_size \\ @default_font_size) do
+  def center(string, {x, y}, font_size \\ @default_font_size, font \\ @default_font) do
     if String.contains?(string, "\n") do
       word_sizes =
         string
         |> String.split("\n")
         |> Enum.map(fn w ->
-          get_font_metrics(w, font_size)
+          get_font_metrics(w, font_size, font)
         end)
 
       width =
@@ -43,14 +50,15 @@ defmodule LayoutOMatic.TextPosition do
 
       {x - trunc(width / 2), y - trunc(height / 2)}
     else
-      {width, height} = get_font_metrics(string, font_size)
+      {width, height} = get_font_metrics(string, font_size, font)
 
       {x - trunc(width / 2), y + trunc(height / 2)}
     end
   end
 
-  defp get_font_metrics(text, font_size) do
-    fm = Scenic.Cache.Static.FontMetrics.get!(@default_font)
+  @doc false
+  defp get_font_metrics(text, font_size, font) do
+    fm = Scenic.Cache.Static.FontMetrics.get!(font)
     ascent = FontMetrics.ascent(font_size, fm)
     fm_width = FontMetrics.width(text, font_size, fm)
 
